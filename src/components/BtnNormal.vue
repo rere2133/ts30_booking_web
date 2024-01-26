7d013970e0f9e4014a0351c13bd746d6c48d1f66
rere2133 <rere2133@users.noreply.github.com>
2024-01-26 23:43:22 +0800

    date picker

---
+++ src/components/BtnNormal.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    @click="$emit('action')"
+    class="tw-group tw-items-center tw-bg-white tw-text-black-100 tw-text-right tw-p-6 tw-rounded-lg hover:tw-bg-primary-100 hover:tw-text-white tw-cursor-pointer tw-w-[97%]"
+  >
+    <div class="tw-whitespace-nowrap mr-2 tw-text-h6">{{ text }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineEmits(["action"]);
+
+defineProps({
+  text: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<style scoped></style>
