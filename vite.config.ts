7ce0dfce68cebc66b6c9973286c1f7bdd5ad9fde
William <william.9150@gmail.com>
2024-01-31 18:53:01 +0800

    [feat] vite config add base property

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from "path"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ts30_booking_web/",
   plugins: [vue()],
-  resolve:{
-    alias:{
-      "@": path.join(__dirname, "src")
-    }
-  }
-})
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
+  },
+});
