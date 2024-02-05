cba57368413a647ab7e7b5aa60bf0ccf3c0fa6ab
leo.zheng <leozheng@ad2iction.com>
2024-02-05 14:42:43 +0800

    temp

--- src/views/member/Profile.vue
+++ src/views/member/Profile.vue
@@ -2,6 +2,14 @@
   <div>Member Profile Page</div>
 </template>
 
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+  import {onMounted} from 'vue';
+  import {useHttp} from '@/plugins/httpAxios';
+  const {_axios} = useHttp();
+  onMounted(() => {
+    _axios.get(`orders`).then((res) => {
+      console.log(res.data);
+    })
+  })
+</script>
 <style scoped></style>
